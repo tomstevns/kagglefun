c159f5685d6e3fff6511f7107d75eb9627e4f932
tomstevns <tomstevns@gmail.com>
2021-10-21 14:21:32 +0200

    With test and train files

---
+++ Tutorials/Iowa_houses/iowaHouses.py
@@ -0,0 +1,15 @@
+# Code you have previously used to load data
+import pandas as pd
+
+# Path of the file to read
+iowa_file_path = 'train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+
+# Set up code checking
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex3 import *
+
+print("Setup Complete")
+
